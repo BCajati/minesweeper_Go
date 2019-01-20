d7ba871f604ae08fca21022f8575faafc2995a1a
Becky Cajati <Becky.Cajati@careerbuilder.com>
2019-01-20 11:39:37 -0500

    finish bomb logic

--- minesweeper.go
+++ minesweeper.go
@@ -2,79 +2,139 @@ package main
 
 import (
 	"fmt"
+	"strconv"
 //	"strings"
 )
 
 type Square struct {
-	X int
-	Y int
 	isBomb bool
 	state string
 }
 
-var (
-	s1 = Square{0, 0, false, "X"}  
-	s2 = Square{0,1,false,"X"}  
-	s3 = Square{0,2,false,"X"} 
-	s4 = Square{1,0,false,"X"} 
-	s5 = Square{1,1,false,"X"} 
-	s6 = Square{1,2,true,"X"} 
-	s7 = Square{2,0,false,"X"} 
-	s8 = Square{2,1,false,"X"} 
-	s9 = Square{2,2,false,"X"} 
-//	board = [][]string{
-//		[]string{"X", "X", "X"},
-//		[]string{"X", "X", "X"},
-//		[]string{"X", "X", "X"},
-//	}
-
-	board = [][]Square{
-		[]Square{s1, s2, s3},
-		[]Square{s4, s5, s6},
-		[]Square{s7, s8, s9},
-	}
+const(
+	numRows = 5
+	numCols = 5
 )
 
+var (
+
+	board[numRows][numCols]Square
+
+)
+
+func createBoard(rows, cols int){
+	var s1 Square
+	for x := 0; x < rows; x++ {
+		for y := 0; y < cols; y++{
+			s1 = Square{false,"X"}
+			board[x][y]= s1
+		}
+	}
+
+	// todo make this random
+	board[1][1].isBomb = true
+	board[3][3].isBomb = true
+	board[4][2].isBomb = true
+}
+
 func showBoard(foundBomb bool){
-	//for i := 0; i < len(board); i++ {
-	//	fmt.Printf("%s\n", strings.Join(board[i], " "))
-	//}
 
 	if foundBomb{
-		fmt.Printf("You Lost!")
+		fmt.Printf("You Lost! \n")
 	}
-		fmt.Printf("%s %s %s \n", board[0][0].state, board[0][1].state, board[0][2].state)
-		fmt.Printf("%s %s %s \n", board[1][0].state, board[1][1].state, board[1][2].state)
-		fmt.Printf("%s %s %s \n", s7.state, s8.state, s9.state)
+	var x,y int
+	for  x=0; x < numRows; x++ {
+		for  y = 0; y < numCols; y++ {
+			fmt.Printf(board[x][y].state)
+		}
+		fmt.Printf("\n")
+	}
 	
+}
+
+func searchRowforBombs(x,y, rowToSearch int) int {
+	var numBombs = 0
+	// if this square is not top row count bombs above
+	var colStart, colEnd int 
+	colStart = y
+	colEnd = y
+	if (y != 0){
+		colStart = y-1
+	}
+	if (y != (numCols -1)) {
+		colEnd = y + 1
+	}
+		var col int
+		for col = colStart; col <= colEnd; col++ {
+			if board[rowToSearch][col].isBomb {
+				numBombs ++
+			}
+		}
+	return numBombs
+}
+
+func findAdjacentBombs(x,y int) int {
+	
+	var numBombs = 0
+	// search row above
+	if (x != 0)	{
+		numBombs = numBombs + searchRowforBombs(x,y, x-1)
+	}
+
+	// search row below
+	if ( x != (numRows-1)) {
+		numBombs = numBombs + searchRowforBombs(x,y, x+1)
+	}
+
+	// search same row
+	// we can ignore this squares state since it will be zero,
+	// otherwise we would have found a bomb here and lost the game
+	numBombs = numBombs + searchRowforBombs(x,y, x)
+	
+	return numBombs
 }
 
 func pickSquare(x,y int) bool {
 
-	//var s Square := board[x][y]
-	// s := board[x][y]
+	fmt.Printf("Selected Square %d,%d \n", x,y)
 
 	if board[x][y].isBomb{
 		board[x][y].state = "*"
 		return true
 	}else{
-		// TODO need to compute values
-		board[x][y].state = "1" 
+		var numberOfAdjacentBombs int
+		numberOfAdjacentBombs = findAdjacentBombs(x,y)
+		board[x][y].state = strconv.Itoa(numberOfAdjacentBombs)
 		return false
 	}
+
 }
 
 func main() {
 	
 	fmt.Println("Welcome to Minesweeper")
+
+	createBoard(numRows, numCols)
 	
 	showBoard(false)
 
-	fmt.Println("Select Square 1,0")
 	var foundBomb = pickSquare(1,0)
+	showBoard(foundBomb)
 
+	foundBomb = pickSquare(3,2)
+	showBoard(foundBomb)
+
+	foundBomb = pickSquare(3,4)
+	showBoard(foundBomb)
+
+	foundBomb = pickSquare(1,4)
 	showBoard(foundBomb)
 
 
+	foundBomb = pickSquare(0,2)
+	showBoard(foundBomb)
+
+	foundBomb = pickSquare(4,2)
+	showBoard(foundBomb)
 
 }
